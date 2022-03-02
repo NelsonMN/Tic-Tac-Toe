cbc8d18e2ddb7b2803e0dd3930e86b8537f3155a
Nelson <nelson.mieszkalski@gmail.com>
2022-03-02 09:20:50 -0500

    Added the winning conditions to the game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Player Factory
 
-const player = (character) => {
-    return {character};
+const player = (name, marker) => {
+    return {name, marker};
 }
 
 // Game Initialization
@@ -23,8 +23,10 @@ const gameBoard = (() => {
         element.addEventListener("click", (event) => {
                 console.log(event.target) // use this to change the inner HTML
                 console.log(index) // use this to relate back to the game board data
-                // want to get rid of pointer events
-                // want to ensure that you can't click it again
+                if (event.target.textContent.length = 0) {
+                    event.target.textContent = activePlayer.marker
+                }
+                // want to get rid of pointer event on squares that have been filled out
                 // want to set the respective X or O given the player's turn
                 // want to switch the players turn
                 // want to have a remaining spots counter
@@ -39,10 +41,24 @@ const gameBoard = (() => {
 
 const game = () => {
     // Players
-    const playerOne = player("X")
-    const playerTwo = player("O")
+    const playerOne = player("Player One", "X")
+    const playerTwo = player("Player Two", "O")
 
+    // activePlayer will refer to playerOne.name
+    // activePlayer.marker will refer to either playerOne.marker or playerTwo.marker
 
+    // There can only be 9 moves => initialize a counter from 9 -> 0
+
+    // need to verify after each move that no one has won -> gameWinning function
+    // winning combinations:
+    [0,1,2]
+    [3,4,5]
+    [6,7,8]
+    [0,3,6]
+    [1,4,7]
+    [2,5,8]
+    [0,4,8]
+    [2,4,6]
 }
 
 
