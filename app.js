2e7aa30987593c13019a0f7b24a71a9187501da7
Nelson <nelson.mieszkalski@gmail.com>
2022-03-07 14:54:39 -0500

    Fixed game play

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-// Player Factory;
-const player = (name, marker) => {
-    return {name, marker}
-}
-
 // Game and Board;
 const game = (()=> {
 
     const gameBoard = (() => {
         board = ["", "", "", "", "", "", "", "", ""]
         
-        // UI:
+        // Board UI:
         const gameboard = document.querySelector(".game-board");
         for (let i = 0; i < board.length; i++) {
             const div = document.createElement("div");
             div.classList.add('game-square')
             gameboard.appendChild(div);
     }
-
 
         // Game Play:
         const squares = document.querySelectorAll(".game-square")
@@ -30,29 +24,56 @@ const game = (()=> {
                     if (!gameOver) {
                         if (remainingMoves > 0) {
                             nextPlayer()
+                            currentPlayer()
                         } else if (remainingMoves == 0) {
-                            console.log("Draw")
+                            resultsDisplay.textContent = `Draw!`
                         }
                     }
                 }
             })
         })
-    
-    
+
+        const againButton = document.getElementById("play-again");
+        againButton.addEventListener("click", () => {
+                squares.forEach((square, index) => {
+                    square.textContent = "";
+                    board[index] = "";
+                    activePlayer = playerOne;
+                    remainingMoves = 9;
+                    gameOver = false;
+                    resultsDisplay.textContent = ""
+                    playerDisplay.textContent = ""
+                }
+            )})
+        
+
         return {board}
+
     })()
 
+    // Player Factory
+    const player = (name, marker) => {
+        return {name, marker}
+    }
 
     const playerOne = player("Player 1", "X");
     const playerTwo = player("Player 2", "O");
 
+    // Results Display
+    const resultsDisplay = document.getElementById("results");
+    const playerDisplay = document.getElementById("player");
+
+    // Initial Variables
     let activePlayer = playerOne;
     let remainingMoves = 9;
     let gameOver = false;
 
-    const nextPlayer = () => activePlayer == playerOne ? activePlayer = playerTwo: activePlayer = playerOne
+    const nextPlayer = () => activePlayer == playerOne ? activePlayer = playerTwo : activePlayer = playerOne
+
+    const currentPlayer = () => playerDisplay.textContent = `${activePlayer.name}'s turn!`
 
     const checkWin = () => {
+
         const winningCombinations = [
             [0,1,2],
             [3,4,5],
@@ -69,12 +90,13 @@ const game = (()=> {
             if (activePlayer.marker === gameBoard.board[[combo[0]]] 
                 && activePlayer.marker === gameBoard.board[[combo[1]]] 
                 && activePlayer.marker === gameBoard.board[[combo[2]]]) {
-                    console.log(`${activePlayer.name} wins!`)
+                    resultsDisplay.textContent = `${activePlayer.name} wins!`
                     gameOver = true
                     return true
             }
         } return false
     }
     
-    return {checkWin, nextPlayer}
+    return {checkWin, nextPlayer, currentPlayer}
+    
 })();
